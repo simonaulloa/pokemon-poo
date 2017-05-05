4046d57786aa3f3a5b3a31529e34a7ad15d669f6
simona <simonaulloa@gmail.com>
2017-05-05 03:44:01 -0300

    pokemon

--- assets/main.js
+++ assets/main.js
@@ -20,15 +20,11 @@ function Pokemon(nombre, color, poderDeAtaque){
   }
 }
 
-
   var ataque1 = document.getElementById("cosa").value
   var ataque2 = document.getElementById("cosa2").value
   var pokemon1 = new Pokemon (cosa, "amarillo", 10)
   var pokemon2 = new Pokemon (cosa2, "rojo", 30)
   
   var res = document.getElementById("resultado");
-  res.innerHTML = "hola";
+  res.innerHTML = "Pokemon 1 ataco a pokemon 2 y pokemon tiene 30 de vida restante";
 }
-
-
-
